ebbc87193ebaf90f50cf2fca0a4761bba356c7b1
dhivya <dhivyaece2015@gmail.com>
2025-01-25 17:53:21 +0530

    lint fix

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -8,7 +7,6 @@ app = Flask(__name__)
 # Load the trained model
 model = joblib.load('model.pkl')
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()  # Expecting JSON data
@@ -16,11 +14,10 @@ def predict():
     prediction = model.predict(input_data)
     return jsonify({'prediction': prediction.tolist()})
 
-
 if __name__ == "__main__":
-    # Run the app on port 5000 (make sure the port is open in the security group)
+    # Run the app on port 5000 (can be any port, just make sure it's open in the security group)
     app.run(
         debug=True,
         host='0.0.0.0',
         port=5000
-    )  # Line breaks to avoid exceeding 79 characters
+    )
