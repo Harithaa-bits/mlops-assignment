1f0c9388db0c974dd48559f051b68c2fd882d2ba
dhivya <dhivyaece2015@gmail.com>
2025-01-25 17:16:37 +0530

    test

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 # Load the trained model
 model = joblib.load('model.pkl')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()  # Expecting JSON data
     prediction = model.predict(np.array(data['input']).reshape(1, -1))  # Reshaping input for prediction
     return jsonify({'prediction': prediction.tolist()})
+
 
 if __name__ == "__main__":
     # Run the app on port 5000 (can be any port, just make sure it's open in the security group)
